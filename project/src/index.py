df31ba220f2d50d17437df043c0b32bb2fea5c1b
Eduard095 <157930196+Eduard095@users.noreply.github.com>
2026-09-21 22:36:44 +0000

    first commit

---
+++ project/src/index.py
@@ -0,0 +1,24 @@
+#index.py
+
+govermentBoost = True
+pot = 0
+counter = 0
+second_counter = 0
+
+
+if __name__ == "__main__":
+    while True:
+        print('''
+Welcome to LISA calulator: 
+Please provide the following
+
+-------------------------------------------------------
+Current holding in your savings
+How much from now on will you be contributing monthly?
+And what is your deposit goal
+-------------------------------------------------------
+
+From this I will be able to tell you how long will it take :)
+
+
+''')
