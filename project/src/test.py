a18bd38fd4e277c505cde3affe79f1b3dcf291ac
Eduard095 <157930196+Eduard095@users.noreply.github.com>
2026-09-24 00:22:52 +0000

    deleted test.py

--- project/src/test.py
+++
@@ -1,7 +0,0 @@
-for x in range(11):
-    if x < 1:
-        print(x, "x is smaller than 1")
-    elif x > 0 and x < 13:
-        print(x, "x is bigger than or equal 1")
-    else:
-        print("what")
